6acd60c3f0656f4b5a23817f6e13210dda57cfc2
Osman <osmanaktrk@gmail.com>
2024-06-03 20:04:24 +0200

    het project begon

---
+++ src/main/java/ehb/be/javaspringopdracht/JavaSpringOpdrachtApplication.java
@@ -0,0 +1,13 @@
+package ehb.be.javaspringopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringOpdrachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringOpdrachtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ehb/be/javaspringopdracht/JavaSpringOpdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package ehb.be.javaspringopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringOpdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
