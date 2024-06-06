12f433e31d02839bb982733eda2c79f185133e49
Osman <osmanaktrk@gmail.com>
2024-06-06 03:28:17 +0200

    kleine fixes

--- src/main/java/ehb/be/javaspringopdracht/model/NewsArticle.java
+++ src/main/java/ehb/be/javaspringopdracht/model/NewsArticle.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
